0d60ad06bfe1e9b860fee975e9c5b8ff06891463
Joel <joel.perez@ibit.pe>
2023-01-14 14:17:44 -0500

    refactor: validacion de estructura de datos

--- Cuentas.Backend.Infraestruture/Sql/04.Sp/SP.sql
+++ Cuentas.Backend.Infraestruture/Sql/04.Sp/SP.sql
@@ -1,150 +1,150 @@
-CREATE OR ALTER     PROC [dbo].[INS_RegistrarUsuario]
-(
-	@Usuario varchar(50),
-	@Password varchar(1025),
-	@FechaCreacion datetime,
-	@UsuarioCreacion varchar(50),
-	@FechaModificacion datetime,
-	@UsuarioModificacion varchar(50)
-
-)
-AS
-BEGIN
-	insert into Usuario
+	CREATE OR ALTER     PROC [dbo].[INS_RegistrarUsuario]
 	(
-	Usuario,
-	Password,
-	FechaCreacion,
-	UsuarioCreacion,
-	FechaModificacion,
-	UsuarioModificacion
+		@Usuario varchar(50),
+		@Password varchar(1025),
+		@FechaCreacion datetime,
+		@UsuarioCreacion varchar(50),
+		@FechaModificacion datetime,
+		@UsuarioModificacion varchar(50)
+
 	)
-	values(
-	@Usuario,
-	@Password,
-	@FechaCreacion,
-	@UsuarioCreacion,
-	@FechaModificacion,
-	@UsuarioModificacion
+	AS
+	BEGIN
+		insert into Usuario
+		(
+		Usuario,
+		Password,
+		FechaCreacion,
+		UsuarioCreacion,
+		FechaModificacion,
+		UsuarioModificacion
+		)
+		values(
+		@Usuario,
+		@Password,
+		@FechaCreacion,
+		@UsuarioCreacion,
+		@FechaModificacion,
+		@UsuarioModificacion
+		)
+	END
+
+
+	GO
+
+	CREATE OR ALTER PROC SEL_BuscarCuenta
+	(
+		@Usuario varchar(50)
 	)
-END
+	AS
+	BEGIN
+		select top 1 * from Usuario where Usuario = @Usuario
+	END
 
+	GO
 
-GO
-
-CREATE OR ALTER PROC SEL_BuscarCuenta
-(
-	@Usuario varchar(50)
-)
-AS
-BEGIN
-	select top 1 * from Usuario where Usuario = @Usuario
-END
-
-GO
-
-CREATE OR ALTER PROC UPD_ActualizarUsuario
-(
-	@Id int,
-	@Password varchar(1025),
-	@FechaModificacion datetime,
-	@UsuarioModificacion varchar(50)
-)
-AS
-BEGIN
+	CREATE OR ALTER PROC UPD_ActualizarUsuario
+	(
+		@Id int,
+		@Password varchar(1025),
+		@FechaModificacion datetime,
+		@UsuarioModificacion varchar(50)
+	)
+	AS
+	BEGIN
 	
-	update Usuario 
-	set
-	Password		   = @Password,
-	FechaModificacion  = @FechaModificacion,
-	UsuarioModificacion= @UsuarioModificacion
-	where Id=@Id
-END
+		update Usuario 
+		set
+		Password		   = @Password,
+		FechaModificacion  = @FechaModificacion,
+		UsuarioModificacion= @UsuarioModificacion
+		where Id=@Id
+	END
 
-GO
+	GO
 
-CREATE OR ALTER PROC SEL_ListarEstadoEstadoProyecto
-(
-	@Page			INT,
-	@Size			INT,
-	@Search			VARCHAR(200) = NULL,
-	@OrderBy		VARCHAR(50) = 'Codigo',
-	@OrderDir		VARCHAR(4) = 'ASC',
-	@TotalGlobal		INT OUTPUT,
-	@TotalFiltered   INT OUTPUT
-)
-AS
-BEGIN
-
-	DECLARE @Skip INT;
-	SET @Skip = (@Size * @Page) - @Size;
-
-	SELECT @TotalGlobal = COUNT(*) FROM EstadoProyecto;
-
-	SELECT @TotalFiltered = COUNT(*) 
-	FROM EstadoProyecto
-	WHERE (@Search IS NULL OR UPPER(Descripcion) LIKE '%' + UPPER(@Search) + '%') 
-
-	SELECT *	
-	FROM EstadoProyecto
-	WHERE (@Search IS NULL OR UPPER(Descripcion) LIKE '%' + UPPER(@Search) + '%') 
-	ORDER BY FechaCreacion DESC, 
-		FechaModificacion DESC
-	OFFSET	@Skip ROWS FETCH NEXT (@Size) ROWS ONLY
-
-END
-
-GO
-
-CREATE OR ALTER PROC INS_RegistrarEstadoProyecto
-(
-	@Estado bit,
-	@Descripcion varchar(50),
-	@FechaCreacion datetime,
-	@UsuarioCrea int,
-	@FechaModificacion datetime,
-	@UsuarioModifica int
-)
-AS
-BEGIN
-
-	insert into EstadoProyecto
+	CREATE OR ALTER PROC SEL_ListarEstadoEstadoProyecto
 	(
-	Estado,
-	Descripcion,
-	FechaCreacion,
-	UsuarioCrea,
-	FechaModificacion,
-	UsuarioModifica)
-	values(
-	@Estado,
-	@Descripcion,
-	@FechaCreacion,
-	@UsuarioCrea,
-	@FechaModificacion,
-	@UsuarioModifica
+		@Page			INT,
+		@Size			INT,
+		@Search			VARCHAR(200) = NULL,
+		@OrderBy		VARCHAR(50) = 'Codigo',
+		@OrderDir		VARCHAR(4) = 'ASC',
+		@TotalGlobal		INT OUTPUT,
+		@TotalFiltered   INT OUTPUT
 	)
+	AS
+	BEGIN
 
-END
+		DECLARE @Skip INT;
+		SET @Skip = (@Size * @Page) - @Size;
 
-GO
+		SELECT @TotalGlobal = COUNT(*) FROM EstadoProyecto;
 
-CREATE OR ALTER PROC UPD_ActualizarEstadoProyecto
-(
-	@Id int,
-	@Estado bit,
-	@Descripcion varchar(50),
-	@FechaModificacion datetime,
-	@UsuarioModifica int
-)
-AS
-BEGIN
+		SELECT @TotalFiltered = COUNT(*) 
+		FROM EstadoProyecto
+		WHERE (@Search IS NULL OR UPPER(Descripcion) LIKE '%' + UPPER(@Search) + '%') 
 
-	update EstadoProyecto 
-	set 
-	Estado			 =@Estado,			 
-	Descripcion		 =@Descripcion,		 
-	FechaModificacion=@FechaModificacion,
-	UsuarioModifica	 =@UsuarioModifica	 
-	where Id =@Id
-END
+		SELECT *	
+		FROM EstadoProyecto
+		WHERE (@Search IS NULL OR UPPER(Descripcion) LIKE '%' + UPPER(@Search) + '%') 
+		ORDER BY FechaCreacion DESC, 
+			FechaModificacion DESC
+		OFFSET	@Skip ROWS FETCH NEXT (@Size) ROWS ONLY
+
+	END
+
+	GO
+
+	CREATE OR ALTER PROC INS_RegistrarEstadoProyecto
+	(
+		@Estado bit,
+		@Descripcion varchar(50),
+		@FechaCreacion datetime,
+		@UsuarioCrea int,
+		@FechaModificacion datetime,
+		@UsuarioModifica int
+	)
+	AS
+	BEGIN
+
+		insert into EstadoProyecto
+		(
+		Estado,
+		Descripcion,
+		FechaCreacion,
+		UsuarioCrea,
+		FechaModificacion,
+		UsuarioModifica)
+		values(
+		@Estado,
+		@Descripcion,
+		@FechaCreacion,
+		@UsuarioCrea,
+		@FechaModificacion,
+		@UsuarioModifica
+		)
+
+	END
+
+	GO
+
+	CREATE OR ALTER PROC UPD_ActualizarEstadoProyecto
+	(
+		@Id int,
+		@Estado bit,
+		@Descripcion varchar(50),
+		@FechaModificacion datetime,
+		@UsuarioModifica int
+	)
+	AS
+	BEGIN
+
+		update EstadoProyecto 
+		set 
+		Estado			 =@Estado,			 
+		Descripcion		 =@Descripcion,		 
+		FechaModificacion=@FechaModificacion,
+		UsuarioModifica	 =@UsuarioModifica	 
+		where Id =@Id
+	END
